290d4c38125c6a121d18f236f2f3bc836b7554a3
GeorgeSte <stefosgiwrgos@gmail.com>
2022-06-09 08:54:45 +0300

    change gm to bads

--- src/views/Farmer.vue
+++ src/views/Farmer.vue
@@ -61,7 +61,7 @@
           End cooldown
         </button>
         <button class="nes-btn huVjiU is-warning orange_c" @click="claim">
-          Claim {{ availableA }} $GM
+          Claim {{ availableA }} $BADS
         </button>
         <!-- </div> -->
       </Vault>
